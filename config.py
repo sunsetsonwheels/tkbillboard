667bd950e603972665200c7f4c7a1d26df240349
Nguyen Thanh Nam <vietbetatester@outlook.com>
2018-07-12 11:31:38 +0800

    config.py configured for updater.py.

---
+++ config.py
@@ -0,0 +1,6 @@
+appName = str("tkbillboard.py")
+appRepo = str("https://github.com/jkelol111/tkbillboard.py.git")
+appExecName = str("billpy.py")
+appDir = str("")
+backupOn = int("0")
+backupDir = str("Nothing")
